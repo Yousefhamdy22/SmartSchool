e648cefa15645721ab9f9065e6f1543a9650791f
Yousefhamdy22 <yh6940717@gmail.com>
2024-03-19 09:19:45 +0200

    first Comment

---
+++ admin/assets/js/enviroment.js
@@ -0,0 +1,7 @@
+export let APIURL = "https://smartsns-001-site2.htempurl.com/";  //   http://localhost:5048/
+
+export class endpoints {
+  static getAllStuAdd = `${APIURL}api/Students`;
+  static getAllTeacherAdd = `${APIURL}api/Teachers`;  //Teachers/Admission
+  static getAllJopAdd = `${APIURL}api/JobAdmission`;
+}
---
+++ assets/js/enviroment.js
@@ -0,0 +1,7 @@
+export let APIURL = "https://smartsns-001-site2.htempurl.com/";
+
+export class endpoints {
+  static getAllStuAdd = `${APIURL}api/Students`;
+  static getAllTeacherAdd = `${APIURL}api/Teachers`;
+  static getAllJopAdd = `${APIURL}api/JobAdmission`;
+}
